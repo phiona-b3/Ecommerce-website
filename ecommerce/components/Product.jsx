bc8e08f35f5e6c905c12048c1307716fa3497994
phiona-b3 <phiona030303@gmail.com>
2023-04-25 19:00:19 +0300

    README.md

--- ecommerce/components/Product.jsx
+++ ecommerce/components/Product.jsx
@@ -9,9 +9,9 @@ const Product = ({ product: { image, name, slug, price } }) => {
         <div className="product-card">
           <img 
             src={urlFor(image && image[0])} //check if we have an image and then return the first one
-          width={250}
-          height={250}
-          className="product-image"
+            width={250}
+            height={250}
+            className="product-image"
           />
           <p className="product-name">{name}</p>
           <p className="product-price">${price}</p>
